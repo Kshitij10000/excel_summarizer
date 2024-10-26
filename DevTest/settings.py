1dad61c7683f04c8817a6dc9f9ff1115f9d4098d
Kshitij10000 <kshitijsarve10000@gmail.com>
2024-10-26 21:05:01 +0530

    added IP address

--- DevTest/settings.py
+++ DevTest/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1' , '34.68.72.196']
 
 # Application definition
 
